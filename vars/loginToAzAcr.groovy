794f27a9906d0e9b878db9669293f22f0bddb14f
George Parks <george@archon.inc>
2025-05-23 00:47:06 -0700

    Fix escaping of variables in loginToAzAcr function for proper execution

--- vars/loginToAzAcr.groovy
+++ vars/loginToAzAcr.groovy
@@ -5,10 +5,10 @@ def call() {
    REPO_USER="00000000-0000-0000-0000-000000000000"
    REPO_TOKEN=\$(az acr login --name ${env.INTERNAL_REGISTRY} --expose-token \
                 --output tsv --query accessToken)
-   echo "$REPO_TOKEN" | cosign login "${env.INTERNAL_REGISTRY}" \
-     -u "$REPO_USER" --password-stdin
-   echo "$REPO_TOKEN" | skopeo login "${env.INTERNAL_REGISTRY}" \
-     -u "$REPO_USER" --password-stdin
+   echo "\$REPO_TOKEN" | cosign login "${env.INTERNAL_REGISTRY}" \
+     -u "\$REPO_USER" --password-stdin
+   echo "\$REPO_TOKEN" | skopeo login "${env.INTERNAL_REGISTRY}" \
+     -u "\$REPO_USER" --password-stdin
    set -x
  """
 }
