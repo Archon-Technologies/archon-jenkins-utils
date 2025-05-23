d779a80395ad0a1e4b5f34150413849155417f51
George Parks <george@archon.inc>
2025-05-23 01:43:12 -0700

    Refactor runTrivyScan function to improve parameter handling and streamline command construction

--- vars/runTrivyScan.groovy
+++ vars/runTrivyScan.groovy
@@ -1,22 +1,31 @@
 def call(Map m) {
  def input = m.input
+ def img = m.img
  def output = m.output
 
- if (!input || !output) {
-  error 'Missing required parameters: input or output'
+ if (!(img || input) || !output) {
+  error 'Missing required parameters: (either img or input) or output'
+ }
+
+ def trivyParams = [
+  '--offline-scan',
+  '--skip-db-update',
+  '--cache-dir /root/.cache/trivy',
+  '--format json',
+  "--output ${output}",
+  '--exit-code 1'
+ ]
+
+ if (img) {
+  //add image to beginning of trivy command
+  trivyParams.add(0, img)
+ } else if (input) {
+  //add input to beginning of trivy command
+  trivyParams.add("--input ${input}")
  }
 
  return sh(
-   script: """
-     trivy image \\
-       --offline-scan \\
-       --skip-db-update \\
-       --cache-dir /root/.cache/trivy \\
-       --input ${input} \\
-       --format json \\
-       --output ${output} \\
-       --exit-code 1
-   """.stripIndent(),
+   script: "trivy image ${trivyParams.join(' ')}",
    returnStatus: true
  )
 }
