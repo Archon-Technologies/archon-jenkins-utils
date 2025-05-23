6cdfaf822218b553be7d4ff59a4e9f5e1f9c11d6
George Parks <george@archon.inc>
2025-05-23 01:15:45 -0700

    Fix type declaration for jobQueue in parallelWithMax function for clarity

--- vars/parallelWithMax.groovy
+++ vars/parallelWithMax.groovy
@@ -7,7 +7,7 @@ def call(Map args = [:]) {
  int maxWorkers = Math.min(maxConcurrent, jobs.size())
 
  // thread-safe queue of all work items
- def jobQueue = new java.util.concurrent.LinkedBlockingQueue<>(jobs.entrySet())
+ def jobQueue = new java.util.concurrent.LinkedBlockingQueue<Map.Entry<String,Closure>>(jobs.entrySet())
  // thread-safe map of results (if you actually need them)
  def results  = new java.util.concurrent.ConcurrentHashMap<String,Object>()
 
